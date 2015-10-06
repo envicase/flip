197a54e9d2085a8bdbccc1f926fdb7b7230f5629
Gyuwon Yi <gyuwon@live.com>
2015-10-06 17:11:20 +0900

    IModel<TId> 인터페이스를 추가합니다.

---
+++ src/Flip/IModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Flip
+{
+    /// <summary>
+    /// 반응형 스트림에 의해 제공되는 모델을 정의합니다.
+    /// </summary>
+    /// <typeparam name="TId">모델 식별자 형식입니다.</typeparam>
+    public interface IModel<TId>
+        where TId : IEquatable<TId>
+    {
+        /// <summary>
+        /// 모델 식별자를 가져옵니다.
+        /// </summary>
+        TId Id { get; }
+    }
+}
