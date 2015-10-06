a0769ec9d50809401590560066e378b280fabca0
Gyuwon Yi <gyuwon@live.com>
2015-10-06 18:22:36 +0900

    ConnectionExtensions 클래스를 추가합니다.

---
+++ tests/Flip.Tests/ConnectionExtensionsTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Reactive.Linq;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Moq;
+using Ploeh.AutoFixture.Xunit2;
+using Xunit;
+
+namespace Flip.Tests
+{
+    using static It;
+    using static Times;
+
+    public class ConnectionExtensionsTest
+    {
+        [Theory AutoData]
+        public async Task EmitRelaysWithModelInstance(User user)
+        {
+            var connection = Mock.Of<IConnection<User, string>>();
+            IObservable<User> observable = null;
+            Mock.Get(connection)
+                .Setup(x => x.Emit(IsAny<IObservable<User>>()))
+                .Callback<IObservable<User>>(p => observable = p);
+
+            connection.Emit(user);
+
+            Mock.Get(connection)
+                .Verify(x => x.Emit(IsAny<IObservable<User>>()), Once());
+            var actual = await observable;
+            actual.Should().BeSameAs(user);
+        }
+    }
+}
