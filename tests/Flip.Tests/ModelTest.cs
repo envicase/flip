4ba094fe9e4191be24049be64b1925beaef51e5e
Gyuwon Yi <gyuwon@live.com>
2015-10-06 17:22:35 +0900

    Model<TId> 클래스를 추가합니다.

---
+++ tests/Flip.Tests/ModelTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using FluentAssertions;
+using Ploeh.AutoFixture.Xunit2;
+using Xunit;
+
+namespace Flip.Tests
+{
+    public class ModelTest
+    {
+        [Theory, AutoData]
+        public void SetsIdCorrectly(string id, string userName, string bio) =>
+            new User(id, userName, bio).Id.Should().BeSameAs(id);
+    }
+}
---
+++ tests/Flip.Tests/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Flip.Tests
+{
+    public class User : Model<string>
+    {
+        public User(string id, string userName, string bio)
+            : base(id)
+        {
+            if (userName == null)
+            {
+                throw new ArgumentNullException(nameof(userName));
+            }
+
+            UserName = userName;
+            Bio = bio;
+        }
+
+        public string UserName { get; }
+
+        public string Bio { get; }
+    }
+}
