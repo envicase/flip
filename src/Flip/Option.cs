3ea6b8672ba1bf65b70c1a5c0761f088c9983a4d
Gyuwon Yi <gyuwon@live.com>
2016-02-10 21:02:00 +0900

    Option<T> 구조체를 추가합니다.
    This resolves #10

---
+++ src/Flip/Option.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Flip
+{
+    public static class Option
+    {
+        public static Option<T> Create<T>(T value) =>
+            value == null ? default(Option<T>) : new Option<T>(value);
+    }
+
+    public struct Option<T> : IEquatable<Option<T>>
+    {
+        private readonly bool _hasValue;
+        private readonly T _value;
+
+        public Option(T value)
+        {
+            _hasValue = value != null;
+            _value = value;
+        }
+
+        public bool HasValue => _hasValue;
+
+        public T ValueOrDefault => _hasValue ? _value : default(T);
+
+        public static implicit operator Option<T>(T value)
+        {
+            return Option.Create(value);
+        }
+
+        public bool Equals(Option<T> other)
+        {
+            return _hasValue == other._hasValue
+                ? _hasValue
+                    ? EqualityComparer<T>.Default.Equals(_value, other._value)
+                    : true
+                : false;
+        }
+    }
+}
---
+++ tests/Flip.Tests/OptionTest.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using FluentAssertions;
+using Ploeh.AutoFixture.Xunit2;
+using Xunit;
+
+namespace Flip.Tests
+{
+    public class OptionTest
+    {
+        [Fact]
+        public void IsValueType() =>
+            typeof(Option<>).IsValueType.Should().BeTrue();
+
+        [Fact]
+        public void ImplicitConverterConvertsNullToOption()
+        {
+            // Arrange
+
+            // Act
+            Option<string> option = null;
+
+            // Assert
+            option.HasValue.Should().BeFalse();
+            option.ValueOrDefault.Should().BeNull();
+        }
+
+        [Theory, AutoData]
+        public void ImplicitConverterConvertsValueToOption(string value)
+        {
+            // Arrange
+
+            // Act
+            Option<string> option = value;
+
+            // Assert
+            option.HasValue.Should().BeTrue();
+            option.ValueOrDefault.Should().Be(value);
+        }
+
+        [Fact]
+        public void CreateReturnsNoneWithNull()
+        {
+            // Arrange
+
+            // Act
+            Option<string> option = Option.Create<string>(null);
+
+            // Assert
+            option.HasValue.Should().BeFalse();
+            option.ValueOrDefault.Should().BeNull();
+        }
+
+        [Theory, AutoData]
+        public void CreateReturnsSomeWithValue(string value)
+        {
+            // Arrange
+
+            // Act
+            Option<string> option = Option.Create(value);
+
+            // Assert
+            option.HasValue.Should().BeTrue();
+            option.ValueOrDefault.Should().Be(value);
+        }
+
+        private class Anonymous { }
+
+        [Fact]
+        public void ImplementsIEquatable()
+        {
+            var sut = default(Option<Anonymous>);
+            sut.Should().BeAssignableTo<IEquatable<Option<Anonymous>>>();
+        }
+
+        [Fact]
+        public void EqualsReturnsTrueWithNoneAndNone()
+        {
+            Option<string> x = null;
+            Option<string> y = null;
+
+            bool actual = x.Equals(y);
+
+            actual.Should().BeTrue();
+        }
+
+        [Fact]
+        public void EqualsReturnsFalseWithNoneAndSome()
+        {
+            Option<string> x = null;
+            Option<string> y = "foo";
+
+            bool actual = x.Equals(y);
+
+            actual.Should().BeFalse();
+        }
+
+        [Fact]
+        public void EqualsReturnsFalseWithSomeAndNone()
+        {
+            Option<string> x = "foo";
+            Option<string> y = null;
+
+            bool actual = x.Equals(y);
+
+            actual.Should().BeFalse();
+        }
+
+        [Theory]
+        [InlineData("foo", "foo", true)]
+        [InlineData("foo", "bar", false)]
+        public void EqualsReturnsEqualityOfValues(
+            string xValue, string yValue, bool expected)
+        {
+            Option<string> x = xValue;
+            Option<string> y = yValue;
+
+            bool actual = x.Equals(y);
+
+            actual.Should().Be(expected);
+        }
+    }
+}
